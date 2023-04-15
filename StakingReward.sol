3756d728800df9c0d137a635bfd0e033ccf2b0be
Rudy <rudy871211@gmail.com>
2023-04-15 22:25:42 +0800

    first commit

---
+++ StakingReward.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./IERC20.sol";
+
+contract StakingRewards{
+    IERC20 public immutable stakingToken;
+    IERC20 public immutable rewardsToken;
+    
+    address public owner;
+
+    uint public duration;
+    uint public finishAt;
+    uint public updateAt;
+    uint public rewardRate;
+    uint public rewardPerTokenStored;
+    mapping (address => uint) public userRewardPerTokenPaid;
+    mapping (address => uint) public rewards;
+
+    uint public totalSupply;
+    mapping (address => uint) public balanceOf;
+    
+    modifier onlyOwner()
+    constructor(address _stakingToken, address _rewardsToken) {
+        owner = msg.sender;
+        stakingToken = IERC20(_stakingToken);
+        rewardsToken = IERC20(_rewardsToken);
+    }
+
+    function setRewardDirection(uint _duration) external{
+        
+    }
+
+    function  notifyRewardAmount(uint _amount) external {
+
+    }
+    function stake (uint _amount) external {}
+
+    function withdraw (uint _amount) external {}
+    
+    function earned (address _account) external view returns (uint) {}
+
+    function getReward (address _account) 
+
+    
+
+}
