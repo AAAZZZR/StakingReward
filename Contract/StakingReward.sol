3dfdee41bf77f06fd85ccacacf4092a953094f1f
Rudy <rudy871211@gmail.com>
2023-04-15 23:27:32 +0800

    145

--- Contract/StakingReward.sol
+++ Contract/StakingReward.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingRewards{
+contract StakingRewards is Ownable{
     IERC20 public immutable stakingToken;
     IERC20 public immutable rewardsToken;
     
@@ -20,7 +21,7 @@ contract StakingRewards{
     uint public totalSupply;
     mapping (address => uint) public balanceOf;
     
-    modifier onlyOwner()
+    
     constructor(address _stakingToken, address _rewardsToken) {
         owner = msg.sender;
         stakingToken = IERC20(_stakingToken);
@@ -31,7 +32,7 @@ contract StakingRewards{
         
     }
 
-    function  notifyRewardAmount(uint _amount) external {
+    function  notifyRewardAmount(uint _amount) external onlyOwner{
 
     }
     function stake (uint _amount) external {}
